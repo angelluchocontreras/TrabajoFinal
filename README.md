99b74cdcb7aa763d45189db56e18393c2ae73dac
angel contreras <angelluchocontreras@gmail.com>
2023-07-26 13:32:14 -0300

    Trabajo Final

---
+++ README.md
@@ -0,0 +1,16 @@
+JavaScript - Trabajo Final
+Este proyecto es un ecommerce como parte del trabajo final del curso de JavaScript. A continuación, se detallarán las nuevas funcionalidades y correcciones implementadas y los cambios realizados.
+
+Novedades y Correcciones
+Responsive para Dispositivos Móviles: Se ha agregado un diseño responsive para que el sitio web se adapte de manera óptima a dispositivos móviles, brindando una experiencia de usuario mejorada en smartphones y tablets.
+
+Corrección de Errores: Se han identificado y corregido varios errores que afectaban el funcionamiento del sitio web, mejorando así la estabilidad y la experiencia del usuario al navegar y utilizar las distintas funcionalidades.
+
+Fetch : Se ha utilizado la Fetch API para realizar solicitudes asíncronas al servidor, lo que ha permitido mejorar el rendimiento y la velocidad de carga de la página
+
+Barra de Búsqueda Funcional: Se ha agregado una barra de búsqueda funcional que permite a los usuarios buscar productos por marca. Los resultados de la búsqueda se muestran de manera dinámica, facilitando la navegación y la elección de productos.
+
+Carpeta JSON para Productos: Los productos ahora están almacenados en un archivo JSON separado
+
+Modificaciones en el Carrito: Se han realizado modificaciones y se agrego librerias para que tenga mas vida la pagina web
+
--- js/main.js
+++ js/main.js
@@ -1,64 +1,124 @@
-
-let productos = [];
-fetch("./js/productos.json")
-.then(Response => Response.json())
-.then(data => {
-	productos = data;
-	cargarProductos(productos)
-})
-
-
-  const productosMostrados = productos.slice();
-  const contenedorProductos = document.querySelector("#contenedor-producto");
-  const botonesCategorias = document.querySelectorAll(".boton-categoria");
-  const tituloPrincipal = document.querySelector("#titulo-principal");
-  let botonesAgregar = document.querySelectorAll(".producto-agregar");
-  const numerito = document.querySelector("#numerito");
-
+const tiendaProductos = {
+	productos: [],
+	contenedorProductos: document.querySelector("#contenedor-producto"),
+	botonesCategorias: document.querySelectorAll(".boton-categoria"),
+	tituloPrincipal: document.querySelector("#titulo-principal"),
+	botonesAgregar: document.querySelectorAll(".producto-agregar"),
+	numerito: document.querySelector("#numerito"),
+	productosEnCarrito: [], 
   
-
-
-  function cargarProductos(productosElegidos) {
-	contenedorProductos.innerHTML = productosElegidos
-	  .map((producto) => `
-		<div class="producto">
-		  <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" />
-		  <div class="producto-detalles">
-			<h3 class="producto-titulo">${producto.titulo}</h3>
-			<p class="producto-precio">${producto.precio}</p>
-			<button class="producto-agregar" id="${producto.id}">Agregar +</button>
+	cargarProductos: function (productosElegidos) {
+	  this.contenedorProductos.innerHTML = productosElegidos
+		.map(
+		  (producto) => `
+		  <div class="producto">
+			<img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" />
+			<div class="producto-detalles">
+			  <h3 class="producto-titulo">${producto.titulo}</h3>
+			  <p class="producto-precio">${producto.precio}</p>
+			  <button class="producto-agregar" id="${producto.id}">Agregar +</button>
+			</div>
 		  </div>
-		</div>
-	  `)
-	  .join("");
+		`
+		)
+		.join("");
   
-	actualizarBotonesAgregar();
-  }
+	  this.actualizarBotonesAgregar();
+	},
   
+	actualizarBotonesAgregar: function () {
+	  this.botonesAgregar = document.querySelectorAll(".producto-agregar");
+	  this.botonesAgregar.forEach((boton) => {
+		boton.addEventListener("click", this.agregarAlCarrito.bind(this));
+	  });
+	},
   
-  botonesCategorias.forEach((boton) => {
+	agregarAlCarrito: function (e) {
+	  const idBoton = e.currentTarget.id;
+	  const productoAgregado = this.productos.find((producto) => producto.id === idBoton);
+  
+	  if (this.productosEnCarrito.some((producto) => producto.id === idBoton)) {
+		this.productosEnCarrito.find((producto) => producto.id === idBoton).cantidad++;
+	  } else {
+		productoAgregado.cantidad = 1;
+		this.productosEnCarrito.push(productoAgregado);
+	  }
+  
+	  Toastify({
+		text: "Producto agregado +",
+		duration: 1500,
+		destination: "../pages/carrito.html",
+		close: false,
+		gravity: "top",
+		position: "right",
+		stopOnFocus: true,
+		style: {
+		  background: "linear-gradient(to right, #3b3b3b, #383838fd )",
+		  borderRadius: "10px",
+		  textTransform: "uppercase",
+		  fontSize: "0.85rem",
+		},
+		offset: {
+		  x: "2.5rem",
+		  y: "2.5rem",
+		},
+		onClick: function () {},
+	  }).showToast();
+  
+	  this.actualizarNumerito();
+	  localStorage.setItem("productos-en-carrito", JSON.stringify(this.productosEnCarrito));
+	},
+  
+	actualizarNumerito: function () {
+	  const nuevoNumerito = this.productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+	  this.numerito.innerText = nuevoNumerito;
+	},
+  
+	obtenerProductosEnCarrito: function () {
+	  const productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+	  if (productosEnCarritoLS) {
+		this.productosEnCarrito = JSON.parse(productosEnCarritoLS);
+		this.actualizarNumerito();
+	  }
+	},
+  
+	guardarProductosEnCarrito: function () {
+	  localStorage.setItem("productos-en-carrito", JSON.stringify(this.productosEnCarrito));
+	},
+  };
+  
+ 
+  fetch("./js/productos.json")
+	.then((Response) => Response.json())
+	.then((data) => {
+	  tiendaProductos.productos = data;
+	  tiendaProductos.cargarProductos(tiendaProductos.productos);
+	});
+  
+
+  tiendaProductos.botonesCategorias.forEach((boton) => {
 	boton.addEventListener("click", (e) => {
-	  botonesCategorias.forEach((boton) => boton.classList.remove("active"));
-  
+	  tiendaProductos.botonesCategorias.forEach((boton) => boton.classList.remove("active"));
 	  e.currentTarget.classList.add("active");
 	  if (e.currentTarget.id != "todos") {
-		const productoCategoria = productos.find(
-		  (producto) => producto.categoria.id === e.currentTarget.id
-		);
-		tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-		const productosBoton = productos.filter(
-		  (producto) => producto.categoria.id === e.currentTarget.id
-		);
-  
-		cargarProductos(productosBoton);
+		const productoCategoria = tiendaProductos.productos.find((producto) => producto.categoria.id === e.currentTarget.id);
+		tiendaProductos.tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+		const productosBoton = tiendaProductos.productos.filter((producto) => producto.categoria.id === e.currentTarget.id);
+		tiendaProductos.cargarProductos(productosBoton);
 	  } else {
-		tituloPrincipal.innerHTML = "Todos los productos";
-		cargarProductos(productos);
+		tiendaProductos.tituloPrincipal.innerHTML = "Todos los productos";
+		tiendaProductos.cargarProductos(tiendaProductos.productos);
 	  }
 	  const mensajeError = document.getElementById("mensajeError");
-    mensajeError.style.display = "none";
+	  mensajeError.style.display = "none";
 	});
   });
+  
+ 
+  tiendaProductos.obtenerProductosEnCarrito();
+  tiendaProductos.actualizarNumerito();
+  
+  
   
 
   
@@ -124,16 +184,17 @@ btnBusqueda.addEventListener("click", buscarPorMarca);
 
 
 
+
 function buscarPorMarca() {
 	const textoBusqueda = document.getElementById("textoBusqueda").value.trim().toLowerCase();
   
 	if (textoBusqueda === "") {
-	  tituloPrincipal.innerText = "Todos los productos";
-	  cargarProductos(productos);
+	  tiendaProductos.tituloPrincipal.innerText = "Todos los productos"; 
+	  tiendaProductos.cargarProductos(tiendaProductos.productos);
 	  return;
 	}
   
-	const productosFiltrados = productos.filter((producto) => {
+	const productosFiltrados = tiendaProductos.productos.filter((producto) => {
 	  const marcaProducto = producto.categoria.marca.toLowerCase();
 	  return marcaProducto.includes(textoBusqueda);
 	});
@@ -146,13 +207,13 @@ function buscarPorMarca() {
 	  const mensajeError = document.getElementById("mensajeError");
 	  mensajeError.style.display = "none";
   
-	  tituloPrincipal.innerText = `Productos de la marca "${textoBusqueda}":`;
+	  tiendaProductos.tituloPrincipal.innerText = `Productos de la marca "${textoBusqueda}":`; 
   
-	  cargarProductos(productosFiltrados);
-	}}
-
- 
-
+	  tiendaProductos.cargarProductos(productosFiltrados);
+	}
+  }
+  
+  
 
 
   
